df4873e0843b027cb078898716f944bcda6ec888
Nirmal-Kotiyal <nirmal.kotiyal@gmail.com>
2020-01-11 17:27:27 +0530

    "Added success propmt"

---
+++ src/components/Sucess.component.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+
+export default class Sucess extends Component{
+
+render(){
+    return(
+     <div className="contactform-text">
+         <h1>Thank you</h1>
+        </div>
+    )
+}
+
+} 
